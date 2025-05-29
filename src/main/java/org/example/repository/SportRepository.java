cf979078c179a8147ac4d07b0dac7a7935caba00
NikaPoliukova <nikanarchuk@gmail.com>
2025-05-29 22:33:54 +0400

    2 task was added

--- src/main/java/org/example/repository/SportRepository.java
+++ src/main/java/org/example/repository/SportRepository.java
@@ -9,4 +9,5 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface SportRepository extends ReactiveCrudRepository<Sport, Integer> {
     Mono<Boolean> existsByName(String name);
+    Flux<Sport> findByNameContainingIgnoreCase(String name);
 }
